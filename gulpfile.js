310964baed146d45072d29f395dfe9c5bc2af6da
zzr <3510789785@qq.com>
2018-11-05 10:00:11 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: zhangzirui
  * @Date: 2018-11-05 08:28:31
  * @Last Modified by: zhangzirui
- * @Last Modified time: 2018-11-05 09:22:09
+ * @Last Modified time: 2018-11-05 09:58:22
  */
 
 // 引入
@@ -20,6 +20,7 @@ var clean = require('gulp-clean-css');
 var babel = require('gulp-babel');
 
 // 生产环境
+// 转换scss
 gulp.task('devSass', function () {
     return gulp.src('./src/scss/*.scss')
             .pipe(sass())
@@ -29,7 +30,7 @@ gulp.task('devSass', function () {
             }))
             .pipe(gulp.dest('./src/css'))
 });
-
+//压缩js
 gulp.task('devUglify', function () {
     return gulp.src('./src/js/*.js')
             .pipe(babel({
@@ -38,11 +39,11 @@ gulp.task('devUglify', function () {
             .pipe(uglify())
             .pipe(gulp.dest('./src/js'))
 });
-
+// 监听scss
 gulp.task('watch', function () {
     return gulp.watch('./src/scss/*.scss', gulp.series('devSass'))
 });
-
+// 起服务器
 gulp.task('server', function () {
     return gulp.src('./src')
             .pipe(webserver({
@@ -53,9 +54,11 @@ gulp.task('server', function () {
                         return res.end('');
                     }
                     var pathname = url.parse(req.url).pathname;
-                    if (/^\/api/.test(pathname)) {
-
-                    } else {
+                    if (/^\/api/.test(pathname)) {// 接口
+                        if (pathname === '/api/load') {
+                            res.end(JSON.stringify({code:0, msg: '成功'}));
+                        }
+                    } else { // 文件
                         pathname = pathname === '/' ? 'index.html' : pathname;
                         res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
                     }
@@ -65,13 +68,14 @@ gulp.task('server', function () {
 
 gulp.task('dev', gulp.series('devSass', 'devUglify', 'server', 'watch'));
 
-// 线上环境
+// 线上环境 打包
+// css
 gulp.task('buildCss', function () {
     return gulp.src('./src/css/*.css')
             .pipe(clean())
             .pipe(gulp.dest('./dist/css'))
 });
-
+// 普通js
 gulp.task('buildUglify', function () {
     return gulp.src('./src/js/*.js')
             .pipe(babel({
@@ -80,16 +84,16 @@ gulp.task('buildUglify', function () {
             .pipe(uglify())
             .pipe(gulp.dest('./dist/js'))
 });
-
+// libs
 gulp.task('copyJs', function () {
     return gulp.src('./src/js/libs/*.js')
             .pipe(babel({
                 presets: ['env']
             }))
             .pipe(uglify())
-            .pipe(gulp.dest('./dist/libs'))
+            .pipe(gulp.dest('./dist/js/libs'))
 });
-
+// 压缩html
 gulp.task('htmlmin', function () {
     return gulp.src('src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
